ba0e8d5b30ea8c6b231af33cca1d90bc97f43fc4
md. yasin <yasinbuetcse14@gmail.com>
2023-05-06 13:55:12 +0600

    update

--- src/Components/Experience/Data.js
+++ src/Components/Experience/Data.js
@@ -1,39 +1,65 @@
+import dp from '../../Images/BUET_LOGO.svg.png'
+import samsung_health from '../../Images/samsung_health.png'
+import samsung_knox from '../../Images/knox.jpg'
+import samsung_logo from '../../Images/samsung_logo.png'
+
+
 const Data = [
     {
-        icon: 'Briefcase',
+        icon: samsung_logo,
         title: 'Senior Software Engineer',
         institution: 'Samsung R&D Institute Bangladesh',
         location: 'Dhaka, Bangladesh',
         time: '01/2023 - Present',
         description: 'Working as a member of NC Experience Group',
-        skills: ['C#', '.Net', 'WinUI3', 'Windows'],
+        skills: ['C#', '.Net', 'WPF', 'Windows'],
+        role: [
+            'Designed Customized Report for Ultrasound Machines using .Net and WPF.',
+            'Designed editor and viewer module using MVVM Architecture.'
+        ],
     },
     {
-        icon: 'Briefcase',
+        icon: samsung_knox,
         title: 'Senior Software Engineer',
         institution: 'Samsung R&D Institute Bangladesh',
         location: 'Dhaka, Bangladesh',
         time: '02/2021 - 01/2023',
         description: 'Working as a member of Wearable Experience Group',
         skills: ['React', 'Redux', 'Electron', 'NodeJS', 'Typescript'],
+        role: [
+            'Built HTTP Relay Server using Node JS to share files with enterprise devices instantly in a closed network.',
+            'Designed configuration profile database with 100+ entries using MongoDB and Express and reduced data redundancy.',
+            'Implemented dynamic UI components with React-Redux to integrate 100+ forms seamlessly and refactored code to increase reusability with 50% reduced code.',
+            'Designed and implemented QR code feature using NodeJS to assimilate custom configuration data to deploy on enterprise devices.',
+            'Implemented License Verification for customers using OpenSSL to block unauthorized installation of the application.',
+            'Optimized app performance by 25% by fixing potential NodeJS memory leaks and CPU usage through memory and CPU profiling.',
+        ],
     },
     {
-        icon: 'Briefcase',
+        icon: samsung_health,
         title: 'Software Engineer',
         institution: 'Samsung R&D Institute Bangladesh',
         location: 'Dhaka, Bangladesh',
         time: '07/2019 - 02/2021',
         description: 'Worked as a member of Samsung Health Application Group',
         skills: ['Swift', 'Objective-C', 'Rest APIs '],
+        role: [
+            'Implemented Data Platform module which validates, authenticates and encrypts sensitive health data produced by the Samsung smart watches.',
+            'Implemented REST APIs for CRUD operations of data such as water, caffeine intake, steps count.',
+            'Solved a critical issue called Central European Summer Time (CEST) issue which causes timestamp conflict of health data.',
+            'Built functional UI components with SwiftUI.',
+            'Contributed in increasing Unit Test Coverage upto 85% of the projects.',
+        ],
     },
     {
-        icon: 'GraduationCap',
+        icon: dp,
         title: 'Undergrad Student',
         institution: 'Bangladesh University of Engineering and Technology',
         location: 'Dhaka, Bangladesh',
         time: '02/2015 - 04/2019',
         description: 'Completed B.Sc in Computer Science and Engineering',
         skills: [],
+        role: [],
     },
 ]
 
--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -1,49 +1,65 @@
-import React from 'react'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGraduationCap, faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
-import Data from './Data';
+import Data from './Data'
 import './Experience.css'
+import dp from '../../Images/BUET_LOGO.svg.png'
+
+const ExperienceElement = ({ data }) => {
+  const [detailsblk, setDetailsblk] = useState(false)
+
+  return (
+    <VerticalTimelineElement
+      className="vertical-timeline-element--work"
+      contentStyle={{ background: '#0A131E', color: '#bcd4e1' }}
+      contentArrowStyle={{ borderRight: '7px solid #0A131E' }}
+      date={data.time}
+      iconStyle={{ background: '#0A131E', color: '#bcd4e1' }}
+      icon={
+        <div className='ExperienceIcon'>
+          <img height='80%' width='80%' src={data.icon} alt='dp' />
+        </div>
+      }
+    >
+      <h3 className="vertical-timeline-element-title" style={{ color: '#bcd4e1' }}>{data.title}</h3>
+      <h4 className="vertical-timeline-element-subtitle" style={{ color: 'aqua' }}>{data.institution}</h4>
+      <h4 className="vertical-timeline-element-subtitle" style={{ color: '#8bd0dd' }}>{data.location}</h4>
+      <p style={{ color: '#66ddaa' }}>
+        {data.description}
+      </p>
+      <div className='Stacks'>
+        {data.skills.map((val, index) => {
+          return <div key={index}>{val}</div>
+        })}
+      </div>
+      <div className='Role' hidden={!detailsblk}>
+        Role:
+        <ul>
+          {data.role.map((val, index) => {
+            return <li key={index}>{val}</li>
+          })}
+        </ul>
+      </div>
+      <div className='DetailsBLK' hidden={data.role.length === 0} onClick={() => setDetailsblk(status => !status)}>
+        {detailsblk ? 'Hide Details' : 'View Details'}
+      </div>
+    </VerticalTimelineElement>
+  );
+};
 
 const Experience = () => {
-  const ExperienceElement = ({ data }) => {
-      return (
-        <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: '#0A131E', color: '#bcd4e1' }}
-            contentArrowStyle={{ borderRight: '7px solid #0A131E' }}
-            date={data.time}
-            iconStyle={{ background: '#0A131E', color: '#bcd4e1' }}
-            icon={<FontAwesomeIcon icon={data.icon === 'GraduationCap' ? faGraduationCap : faBriefcase}/>}
-        >
-            <h3 className="vertical-timeline-element-title" style={{color: '#bcd4e1'}}>{data.title}</h3>
-            <h4 className="vertical-timeline-element-subtitle" style={{color: 'aqua'}}>{data.institution}</h4>
-            <h4 className="vertical-timeline-element-subtitle" style={{color: '#8bd0dd'}}>{data.location}</h4>
-            <p style={{color: '#66ddaa'}}>
-                {data.description}
-            </p>
-            <div className='Stacks'>
-              {data.skills.map((val, index) => {
-                return <div key={index}>{val}</div>
-              })}
-            </div>
-        </VerticalTimelineElement>
-      );
-  };
-  
   return (
     <section className='Experience' id='experience'>
-        <div className='Title'>
-          Let me show you my journey!!!
-        </div>
-        <div className='circle' />
-        <VerticalTimeline>
-            {Data.map((val, index) => {
-              return <ExperienceElement key={index} data={val} />
-            })}
-        </VerticalTimeline>
-        <div className='circle' />
+      <div className='Title'>
+        Let me show you my journey!!!
+      </div>
+      <div className='circle' />
+      <VerticalTimeline>
+        {Data.map((val, index) => {
+          return <ExperienceElement key={index} data={val} />
+        })}
+      </VerticalTimeline>
+      <div className='circle' />
     </section>
   )
 }
